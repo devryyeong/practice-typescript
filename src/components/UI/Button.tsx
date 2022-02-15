bea625e22f1d434bba626fc5f89fc4e813c3f18b
devryyeong <thisisme20@daum.net>
2022-02-16 00:06:02 +0900

    styled-components

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import styled from 'styled-components';
-import GlobalStyle from '../../assets/styles/global-style'
-import { theme } from '../../assets/styles/theme'
 
 
 
